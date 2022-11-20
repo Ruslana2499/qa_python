6ee042ff14b1541f1f0b10910d13684a03412d94
RuslanaManbatchurina <ruslanany66@gmail.com>
2022-11-20 20:52:06 +0500

    review changes

--- tests.py
+++ tests.py
@@ -29,7 +29,7 @@ class TestBooksCollector:
         # добавляем новую книгу
         collector.add_new_book('Гордость и предубеждение и зомби')
         # проверяем, что рейтинг у книги равен 1
-        assert collector.books_rating.get('Гордость и предубеждение и зомби') == 1
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
 
     def test_add_two_books_with_the_same_title(self): # Добавить книгу. Добавить книгу с таким же название. В словаре только 1 книга
         # создаем экземпляр (объект) класса BooksCollector
@@ -48,7 +48,7 @@ class TestBooksCollector:
         # меняем рейтинг на 5
         collector.set_book_rating('Гордость и предубеждение и зомби', 5)
         # проверяем, что рейтинг равен 5
-        assert collector.books_rating.get('Гордость и предубеждение и зомби') == 5
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 5
 
     def test_set_book_rating_set_an_inappropriate_rating_it_will_remain_unit(self): #Добавим книгу. Поменяем ей рейтинг на 5.5
         # создаем экземпляр (объект) класса BooksCollector
@@ -58,7 +58,7 @@ class TestBooksCollector:
         # меняем рейтинг на 5.5
         collector.set_book_rating('Гордость и предубеждение и зомби', 5.5)
         # проверяем, что рейтинг равен 1
-        assert collector.books_rating.get('Гордость и предубеждение и зомби') == 1
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
 
     def test_set_book_rating_set_rating_of_more_than_ten_it_will_remain_a_unit(self):  # Добавим книгу. Поменяем ей рейтинг на 11
         # создаем экземпляр (объект) класса BooksCollector
@@ -68,7 +68,7 @@ class TestBooksCollector:
         # меняем рейтинг на 11
         collector.set_book_rating('Гордость и предубеждение и зомби', 11)
         # проверяем, что рейтинг равен 1
-        assert collector.books_rating.get('Гордость и предубеждение и зомби') == 1
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
     def test_set_book_rating_set_rating_to_less_than_one_it_will_remain_a_unit(self):  # Добавим книгу. Поменяем ей рейтинг на 0
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
@@ -77,15 +77,15 @@ class TestBooksCollector:
         # меняем рейтинг на 0
         collector.set_book_rating('Гордость и предубеждение и зомби', 0)
         # проверяем, что рейтинг равен 1
-        assert collector.books_rating.get('Гордость и предубеждение и зомби') == 1
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
 
     def test_get_book_rating_to_find_out_the_rating_book_that_has_not_been_added_to_the_dictionary_there_is_no_rating(self):  #Добавляем книгу. Выводим рейтинг у недобавленной в словарь книги. Рейтинга нет.
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
         # добавляем новую книгу
         collector.add_new_book('Гордость и предубеждение и зомби')
-        # проверяем, что рейтинга нет
-        assert collector.books_rating.get('Как правильно кушать тортик') == None
+        # проверяем, что рейтинг равен None у недобавленноей книги
+        assert collector.get_book_rating('Как правильно кушать тортик') == None
 
     def test_set_book_rating_set_rating_for_book_that_has_not_been_added_to_the_dictionary_there_is_no_rating(self):  # Добавляем 2 книги. Меняем рейтинг другой книге(ее не добавляли в список). Рейтинг не поменяется у несуществующей книги.
         # создаем экземпляр (объект) класса BooksCollector
@@ -95,8 +95,8 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         # меняем рейтинг на 5
         collector.set_book_rating('Как правильно кушать тортик', 5)
-        # проверяем, что рейтинг равен None
-        assert collector.books_rating.get('Как правильно кушать тортик') == None
+        # проверяем, что рейтинг равен None у недобавленноей книги
+        assert collector.get_book_rating('Как правильно кушать тортик') == None
     def test_get_books_with_specific_rating_all_books_with_rating_of_one_will_be_displayed(self):  # Добавить 3 книги. Выводим список книг с рейтингом 1. Выведутся 3 книги
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
@@ -116,7 +116,7 @@ class TestBooksCollector:
         collector.add_new_book('Как правильно кушать тортик')
         # меняем рейтинг на 5
         collector.set_book_rating('Что делать, если ваш кот хочет вас убить', 5)
-        # проверяем, что книг с рейтингом равным единице 3
+        # проверяем, что книг с рейтингом равным пяти 1
         assert len(collector.get_books_with_specific_rating(5)) == 1
     def test_get_books_with_specific_change_the_rating_of_books_books_with_rating_of_six_will_not_be_displayed(self):  # Добавить 3 книги. Установить 1 книге рейтинг 2, второй 3, третей 7. Вывести книги с рейтингом 6. Не выведутся книги.
         # создаем экземпляр (объект) класса BooksCollector
@@ -173,6 +173,7 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         # добавляем книгу в избранное
         collector.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
+        # добавляем эту же книгу еще раз в избранное
         collector.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
         # список favorites содержит 1 книгу
         assert len(collector.get_list_of_favorites_books()) == 1
@@ -180,7 +181,7 @@ class TestBooksCollector:
     def test_delete_book_from_favorites_the_book_is_being_deleted(self): #Добавляем 2 книги. Добавляем их в избранное. В избранном 2 книги. Удаляем одну из избранного. В избранном 1 книга.
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-        # добавляем книгу
+        # добавляем книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         # добавляем книги в избранное
@@ -200,7 +201,7 @@ class TestBooksCollector:
         # добавляем книги в избранное
         collector.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
-        # удаляем книгу из избранного
+        # удаляем книги из избранного
         collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
         collector.delete_book_from_favorites('Что делать, если ваш кот хочет вас убить')
         # список favorites содержит 0 книг, список get_books_rating содержит 2 книги
